793fdd5ee3129fb5f00e067d1ec59e67ae1547ad
Fatima Rizvi <fatimarzv23@gmail.com>
2020-09-11 11:27:18 -0500

    worked on character component and app.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,29 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
+
+
+// const Character = ({ info }) => (
+//     <div className='character'>
+//       {info.name}
+//     </div>
+//   )
+
+export default function Character( props ) {
+    const { chFacts } = props
+    console.log(chFacts)
+
+    return (
+        <div className = 'character'>
+            <h3>{chFacts.name}</h3>
+            <div className = 'ch-attrs'>
+                <p>Gender: {chFacts.gender}</p>
+                <p>Birth Year: {chFacts.birth_year}</p>
+                <p>Height: {chFacts.height}</p>
+                <p>Weight: {chFacts.weight}</p> 
+            </div>
+        </div>
+
+    )
+
+}
